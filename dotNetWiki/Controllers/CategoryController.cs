24cbc77c0dc86e826a4ee99fc5fc11900665ae81
mrrey <mrrey@DESKTOP-GN9QHA2>
2016-11-17 14:59:12 +0100

    Added Basic Category Controller & Modified Layout slightly

---
+++ dotNetWiki/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using dotNetWiki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotNetWiki.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = new WikiContext().Pages.GroupBy(o => o.Category).ToList();
+            return View(categories);
+        }
+    }
+}
